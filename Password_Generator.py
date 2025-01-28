57e1368a99dd75f23395911f130a9efb80be0dd3
Dhanonjoy Saha Anogh <104628129+DjSahaAnogh@users.noreply.github.com>
2025-01-28 09:12:49 +0600

    Password Generator

---
+++ Password_Generator.py
@@ -0,0 +1,31 @@
+import random
+def pass_lenght() -> int:
+    while True: 
+        try:
+            length: int = int(input("Enter the length of password: "))
+            return length
+        except ValueError:
+            print("Please enter a valid number")
+            continue
+def pass_generator(pass_len: int) -> str:
+    caps_letters: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    small_letters: str = "abcdefghijklmnopqrstuvwxyz"
+    digits: str = "0123456789"
+    symbols: str = "!@#$%^&*()_+"
+    password: str = random.choice(caps_letters) + random.choice(small_letters) + random.choice(digits) + random.choice(symbols)
+    if pass_len-4 == 0:
+        password_list: list = list(password)
+        random.shuffle(password_list)
+        password = "".join(password_list)
+        return password
+    else:
+        for i in range(pass_len - 4):
+            password += random.choice(caps_letters + small_letters + digits + symbols)
+        password_list: list = list(password)
+        random.shuffle(password_list)
+        password = "".join(password_list)
+        return password
+if __name__ == "__main__":
+    length: int = pass_lenght()
+    password: str = pass_generator(length)
+    print(f"Your generated password is: {password}")
