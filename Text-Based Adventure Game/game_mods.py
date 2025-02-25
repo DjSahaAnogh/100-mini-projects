cf774554f05cba9a41deb2a69ebbb186e9119e66
Dhanonjoy Saha Anogh <104628129+DjSahaAnogh@users.noreply.github.com>
2025-02-25 19:05:35 +0600

    Text-Based Adventure Game 1

---
+++ Text-Based Adventure Game/game_mods.py
@@ -0,0 +1,8 @@
+def woods_adventure() -> None:
+    print("You are in the woods!")
+
+def castle_adventure() -> None:
+    print("You are in the castle!")
+
+def space_adventure() -> None:
+    print("You are in space!")
---
+++ Text-Based Adventure Game/main-game.py
@@ -0,0 +1,12 @@
+from game_mods import woods_adventure, castle_adventure, space_adventure
+def game() -> None:
+    print("Welcome to the game!")
+    game_choice = input("1.Woods Adventure \n2.Castle Adventure? \n3.Space Adventure? \n4.Exit Game \nMake your choice(Enter 1 through 4 to choise a option): ")
+    if game_choice == "1":
+        woods_adventure()
+    elif game_choice == "2":
+        castle_adventure()
+    elif game_choice == "3":
+        space_adventure()
+    elif game_choice == "4":
+        print("See you  again!")
